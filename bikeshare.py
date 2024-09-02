e5e2bdd791737108d3082acc43cc2a012cacfbae
schwarz, klaus <klaus.schwarz@bshg.com>
2024-09-02 11:03:29 +0200

    refactoring: intro start(txt) and substitute call to start() ofr redundant print and timer start

--- bikeshare.py
+++ bikeshare.py
@@ -73,12 +73,13 @@ def load_data(city, month, day):
         df = df.loc[df['day_of_week'].str.lower() == day]
     return df
 
+def start(txt):
+    print('\n{}\n'.format(txt))
+    return time.time()
 
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
-
-    print('\nCalculating The Most Frequent Times of Travel...\n')
-    start_time = time.time()
+    start_time = start("Calculating the Most Frequent Times of Travel...")
 
     # Display the most common month
     mcv_month = df['month'].mode()
@@ -100,9 +101,7 @@ def time_stats(df):
 
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
-
-    print('\nCalculating The Most Popular Stations and Trip...\n')
-    start_time = time.time()
+    start_time = start("Calculating the Most Popular Stations and Trip...")
 
     mcv_start_station = df["Start Station"].mode()
     print("most common Start Station: {}".format(mcv_start_station.tolist()[0]))
@@ -120,9 +119,7 @@ def station_stats(df):
 
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
-
-    print('\nCalculating Trip Duration...\n')
-    start_time = time.time()
+    start_time = start("Calculating Trip Duration...")
 
     # Display the total trips duration
     total_duration = df["Trip Duration"].sum()
@@ -135,12 +132,9 @@ def trip_duration_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-
 def user_stats(df):
     """Displays statistics on bikeshare users."""
-
-    print('\nCalculating User Stats...\n')
-    start_time = time.time()
+    start_time = start("Calculating User Stats...")
 
     # Display user type counts
     user_type_counts = df["User Type"].value_counts()
